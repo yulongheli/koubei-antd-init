8c908d265ab8adf15887aab56d48320319d1821e
赵伟 <zhaowei@zhaoweideMacBook-Pro.local>
2017-02-17 13:22:55 +0800

    refactor

--- src/component/shop/ShopEntity.js
+++ src/component/shop/ShopEntity.js
@@ -1,28 +1,36 @@
 import { sillyRandomId } from '../../common/utils';
 const EmptyShop = {
 	"address":"上海市浦东新校区民生路1199弄",
-	"bdNickName":"",
-	"bdRealName":"",
-	"brokerName":"",
-	"brokerStaff":"",
-	"category":"",
+	"addressDesc":"地址Memo",
+	"authorizationPictureList":[{"id":"8T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC","url":"http://dl.django.t.taobao.com/rest/1.0/image?fileIds=8T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC&token=3KgBye8YLzV5G0sn0nKxzQABUYAAAAFaSeZY9gAAACMAAQED&timestamp=1487306999101&acl=5625e49e1d709a0e46cb0a088b41a771&zoom=original"}],
+	"bindingPublic":false,
+	"brandId":"2015042000007200",
+	"brandName":"1980 Cafe",
+	"businessTime":"1-7 09:00-23:00",
+	"canModifyCategory":false,
+	"categoryId":"2015050700000010",
+	"categoryIds":["2015050700000000","2015050700000002","2015050700000010"],
+	"certificatePictureList":[{"id":"6T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC","url":"http://dl.django.t.taobao.com/rest/1.0/image?fileIds=6T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC&token=3KgBye8YLzV5G0sn0nKxzQABUYAAAAFaSeZY9gAAACMAAQED&timestamp=1487306999101&acl=64d604fab880b714a0803f8d471ee8a4&zoom=original"}],
+	"certificateValidTime":"2025-08-08",
+	"cityId":"310100",
 	"cityName":"上海市",
-	"competitorDis":"",
-	"createTime":1484275629000,
+	"coverList":[{"id":"2T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC","url":"http://dl.django.t.taobao.com/rest/1.0/image?fileIds=2T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC&token=3KgBye8YLzV5G0sn0nKxzQABUYAAAAFaSeZY9gAAACMAAQED&timestamp=1487306999101&acl=de977372c9e39b86e13544d928ffc6d4&zoom=original"}],
+	"districtId":"310115",
 	"districtName":"浦东新区",
-	"mallId":"",
-	"mallName":"",
-	"merchantName":"cc123",
-	"merchantPid":"2088102146931393",
-	"mobile":"18888888888",
-	"orderId":"",
-	"provinceName":"上海",
-	"qualityScore":"0",
-	"shopId":"",
-	"shopName":"headShopName_1484275571411(shopName)",
-	"shopType":"COMMON",
-	"status":"OPEN",
-}
+	"extInfo":{
+		"isSupportH5":"true",
+		"jobId":"1000000010017596",
+		"opRole":"S_STAFF"
+	},
+	"headShopName":"headShopName_1484275571411",
+	"latitude":"31.226948",
+	"licenseName":"经营许可证",
+	"licensePictureList":[{"id":"7T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC","url":"http://dl.django.t.taobao.com/rest/1.0/image?fileIds=7T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC&token=3KgBye8YLzV5G0sn0nKxzQABUYAAAAFaSeZY9gAAACMAAQED&timestamp=1487306999101&acl=b90c301ecb2b1bf3dae6fce559b486eb&zoom=original"}],"licenseSeq":"license_1484275571411","licenseValidTime":"2025-08-09","logoList":[{"id":"1T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC","url":"http://dl.django.t.taobao.com/rest/1.0/image?fileIds=1T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC&token=3KgBye8YLzV5G0sn0nKxzQABUYAAAAFaSeZY9gAAACMAAQED&timestamp=1487306999101&acl=016e70350b818b883dd08e7dde597995&zoom=original"}],"longitude":"121.549728","mobileNo":"18888888888","otherAuthorizationList":[{"id":"9T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC","url":"http://dl.django.t.taobao.com/rest/1.0/image?fileIds=9T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC&token=3KgBye8YLzV5G0sn0nKxzQABUYAAAAFaSeZY9gAAACMAAQED&timestamp=1487306999101&acl=0fa5cfb8c618436721eb3d7e9ff1218d&zoom=original"},{"id":"10T8Pp00AT7eo9NoAJkMR3AAAACMAAQE","url":"http://dl.django.t.taobao.com/rest/1.0/image?fileIds=10T8Pp00AT7eo9NoAJkMR3AAAACMAAQE&token=3KgBye8YLzV5G0sn0nKxzQABUYAAAAFaSeZY9gAAACMAAQED&timestamp=1487306999101&acl=7f64ce939ba9a4d286091688a5c36f97&zoom=original"}],"otherService":"其他服务信息","outShopId":"outShopId_1484275571411","partnerId":"2088102146931393","payType":"ONLINE_PAY","perPay":"89","pictureDetailList":[{"id":"3T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC","url":"http://dl.django.t.taobao.com/rest/1.0/image?fileIds=3T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC&token=3KgBye8YLzV5G0sn0nKxzQABUYAAAAFaSeZY9gAAACMAAQED&timestamp=1487306999101&acl=2789f892fa26e048f74c3f8aee6a40b2&zoom=original"},{"id":"4T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC","url":"http://dl.django.t.taobao.com/rest/1.0/image?fileIds=4T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC&token=3KgBye8YLzV5G0sn0nKxzQABUYAAAAFaSeZY9gAAACMAAQED&timestamp=1487306999101&acl=c7134f4b8c045abdea910ab38d3bee5f&zoom=original"},{"id":"5T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC","url":"http://dl.django.t.taobao.com/rest/1.0/image?fileIds=5T8Pp00AT7eo9NoAJkMR3AAAACMAAQEC&token=3KgBye8YLzV5G0sn0nKxzQABUYAAAAFaSeZY9gAAACMAAQED&timestamp=1487306999101&acl=0671cbfac604946c70ed4edcd8f5cd4c&zoom=original"}],"provinceId":"310000","provinceName":"上海","receiveUserId":"2088102146931393","services":{"box":"false","noSmoke":"false","park":"true","wifi":"false"},
+	"shopId":"2017011300077000000003496988",
+	"shopName":"shopName",
+	"shopProblemLabels":{"addressErrorReasons":[],"phoneNumberErrorReasons":[],"shopNameErrorReasons":[]},"shopType":"COMMON"
+};
+
 export default class ShopEntity {
 	constructor(options) {
 		Object.assign(this, EmptyShop, options, {
