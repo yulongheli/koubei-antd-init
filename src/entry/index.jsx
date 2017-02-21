97652b3092ee18f7f2321ba08cb9cbf09636d769
楚吴 <william.zw@koubei.com>
2017-02-21 15:13:31 +0800

    style(compile): eslint
    eslint

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'babel-polyfill';
-import {Router, Route, hashHistory} from 'react-router';
+import { Router, Route, hashHistory } from 'react-router';
 import userRoutes from '../component/user/routes';
 import shopRoutes from '../component/shop/routes';
 
-const RouteArray = [
-	{
-		path: '/',
-		onEnter: (nextState, replace) => replace('/user/login')
-	}
-]
-.concat(userRoutes)
-.concat(shopRoutes);
+const RouteArray = [{
+  path: '/',
+  onEnter: (nextState, replace) => replace('/user/login'),
+}]
+  .concat(userRoutes)
+  .concat(shopRoutes);
 
-const RouteCollection = RouteArray.map((props, index) => {
-  return <Route {...props} key={index} />;
-});
+const RouteCollection = RouteArray.map((props, index) =>
+  <Route {...props} key={index} />
+);
 
 ReactDOM.render(<div>
-  <div style={{width: 998, margin: '0 auto', position: 'relative'}}>
+  <div style={{ width: 998, margin: '0 auto', position: 'relative' }}>
     <Router history={hashHistory}>
       {RouteCollection}
     </Router>
